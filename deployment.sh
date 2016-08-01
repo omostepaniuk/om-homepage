1909094841a1d810a241973e261fe7d286eedd69
omostepaniuk <o.mostepaniuk@gmail.com>
2016-08-01 15:19:36 +0300

    Clean up deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# bash ftp deployment script
+# ftp deployment script
 # version 1.0.0
 
-# check if all configuration options have been provided
-# upload
-
-# source directory to be uploaded (contents without a directory itself)
 SOURCE_DIR=./_site
 
 declare -A DEPLOYMENT_SETTINGS
@@ -26,16 +22,11 @@ function parse_config(){
     if [[ $line =~ ^"["(.+)"]"$ ]]; then
       section=${BASH_REMATCH[1]}
     elif [[ $line =~ ^([_[:alpha:]][_[:alnum:]]*)"="(.*) ]]; then
-      #declare ${arrname}[${BASH_REMATCH[1]}]="${BASH_REMATCH[2]}"
       if [[ ${TARGET_ENVIRONMENT,,} == ${section,,} ]]; then
         read DEPLOYMENT_SETTINGS[${BASH_REMATCH[1]}] <<< "${BASH_REMATCH[2]}"
       fi
     fi
   done < $1
-
-  # test it
-  #for i in "${!STAGING[@]}"; do echo "$i ==> ${STAGING[$i]}"; done
-  #echo length: ${#STAGING[@]}
 }
 
 if [[ $# < 2 ]]; then
@@ -43,7 +34,6 @@ if [[ $# < 2 ]]; then
   exit 1
 fi
 
-# parsing command-line options
 while [[ $# > 1 ]]
 do
   key="$1"
@@ -59,20 +49,17 @@ do
     ;;
     *)
       print_help
-      # TODO can we exit the script from within while loop?
       exit 1
     ;;
   esac
   shift
 done
 
-# checking correct target environment has been specified
 if [[ $TARGET_ENVIRONMENT != "staging" && $TARGET_ENVIRONMENT != "production" ]]; then
   echo "Unknown type of target environment specified. Either 'staging' or 'production' must be used with -e option"
   exit 1
 fi
 
-# reading configuration
 parse_config $CONFIG_FILE
 
 FTP_USER=${DEPLOYMENT_SETTINGS[FTP_USER]}
@@ -81,4 +68,4 @@ FTP_DIR=${DEPLOYMENT_SETTINGS[FTP_TARGET_DIR]}
 
 lftp ftp://$FTP_USER@$FTP_HOST -e "set ftp:ssl-allow no; mirror --exclude .htaccess --delete-first -R $SOURCE_DIR $FTP_DIR ; quit"
 
-echo "Success!"
+echo "Deployment complete"
